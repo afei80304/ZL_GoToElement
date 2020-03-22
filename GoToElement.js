a89090044da06b10c2010cbc7ace8096a002f404
afei80304 <afei80304@gmail.com>
2020-03-22 14:44:59 +0800

    完成滾輪動畫

--- GoToElement.js
+++ GoToElement.js
@@ -5,9 +5,17 @@ $("*").each(function () {
         if(target != undefined){
 
             var duration = $(this).attr("data-goto-duration");
+            //上方位置=目標屬性.位移.上方
             var top = $(target).offset().top;
-    
-            $("html, body").animate({scrollTop: top},parseInt(duration));
+            //動畫({捲動上方:上方位置}，時間)
+            $("html, body").stop().animate({scrollTop: top},parseInt(duration));
         }
     }); 
+});
+//當捲動滾輪時停止動畫
+//jQuery事件: mousewheel 滑鼠滾動
+$(window).on("mousewheel",function(){
+    $("*").each(function(){
+        $(this).stop();
+    });
 });
