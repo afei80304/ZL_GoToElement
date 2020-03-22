fe582fb4930d0c760a82982bb2202630812fbfd2
afei80304 <afei80304@gmail.com>
2020-03-22 13:06:38 +0800

    完成指定動畫

---
+++ GoToElement.js
@@ -0,0 +1,13 @@
+$("*").each(function () {
+    $(this).click(function () { 
+        var target = $(this).attr("data-goto-target");
+
+        if(target != undefined){
+
+            var duration = $(this).attr("data-goto-duration");
+            var top = $(target).offset().top;
+    
+            $("html, body").animate({scrollTop: top},parseInt(duration));
+        }
+    }); 
+});
